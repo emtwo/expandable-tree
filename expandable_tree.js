aaafeb7bdd72ee12612ab0e6863b546a2c405202
Marina Samuel <msamuel@mozilla.com>
2014-07-08 11:12:13 -0400

    Make nodes expandable.

--- expandable_tree.js
+++ expandable_tree.js
@@ -17,9 +17,8 @@ var mainNode = {"id": 0,
                 "fixed": true,
                 "radius": MAIN_RADIUS }
 
-var nodes = [mainNode], links = [], nodeList = {};
+var originalNodes = [mainNode], links = [], nodeList = {};
 populateGraphDFS(categories, 0);
-scaleGraphWeights();
 createNodeList();
 
 var force = d3.layout.force()
@@ -31,10 +30,10 @@ svg.append("rect")
     .attr("width", width)
     .attr("height", height);
 
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
-var labels;
+var link, node;
 
+var nodes = originalNodes;
+scaleGraphWeights();
 update();
 
 function update() {
@@ -43,37 +42,36 @@ function update() {
     .links(links)
     .start();
 
-  link = link.data(links)
-    .enter().append("line")
+  link = svg.selectAll(".link")
+  link = link.data(links);
+  link.exit().remove();
+  link.enter().append("line")
     .attr("class", "link");
 
-  var gnodes = svg.selectAll('g.gnode')
-    .data(nodes)
-    .enter()
-    .append('g')
-    .classed('gnode', true);
-    
-  var drag = force.drag()
-    .on("dragstart", dragstart);
+  node = svg.selectAll(".node");
+  node = node.data(nodes, function(d) { return  d.id; });
+  node.exit().remove();
+  node.enter().append("g")
+    .attr("class", "node")
+    .on("click", click)
+    .call(force.drag);
 
-  node = gnodes.append("circle")
-      .attr("class", "node")
-      .on("click", click)
-      .attr("r", function(d) { return d.radius; } )
-      .style('fill', function(d) { return  colors(d.id); })
-      .call(drag);
+  node.append("circle")
+    .attr("class", "node")
+    .attr("r", function(d) { return d.radius; } )
+    .style('fill', function(d) { return  colors(d.id); })
 
-  labels = gnodes.append("text")
-      .text(function(d) { return d.name; })
-      .style("font-size", "1px")
-      .each(getSize)
-      .style("font-size", function(d) { return d.scale + "px"; })
+  node.append("text")
+    .text(function(d) { return d.name; })
+    .style("font-size", "1px")
+    .each(getSize)
+    .style("font-size", function(d) { return d.scale + "px"; })
 }
 
 function populateGraphDFS(root, parentID) {
   if (Object.keys(root).length > 0) {
     for (var child in root) {
-      var currLength = nodes.length;
+      var currLength = originalNodes.length;
       var weight = root[child]["weight"]
       if (weight < minWeight) {
         minWeight = weight;
@@ -83,7 +81,7 @@ function populateGraphDFS(root, parentID) {
       }
 
       var hasChildren = root[child]["children"];
-      nodes.push({"id": currLength, "radius": weight, "name": child, "hasChildren": hasChildren});
+      originalNodes.push({"id": currLength, "radius": weight, "name": child});
       links.push({"source": parentID, "target": currLength});
       if (hasChildren) {
         populateGraphDFS(root[child]["children"], currLength);
@@ -95,8 +93,8 @@ function populateGraphDFS(root, parentID) {
 function scaleGraphWeights() {
   var oldRange = (maxWeight - minWeight);
   var newRange = (MAX_NODE_RADIUS - MIN_NODE_RADIUS);
-  for (var i in nodes) {
-    var node = nodes[i];
+  for (var i in originalNodes) {
+    var node = originalNodes[i];
     if (node.id == 0) continue;
     node["radius"] = (((node["radius"] - minWeight) * newRange) / oldRange) + 30;
   }
@@ -113,6 +111,11 @@ function createNodeList() {
 }
 
 function click(d) {
+  if (d3.event.defaultPrevented) return; // click suppressed
+
+  if (!nodeList[d.id]) {
+    return; // leaf nodes.
+  }
   if (nodeList[d.id].children) {
     nodeList[d.id]._children = nodeList[d.id].children;
     nodeList[d.id].children = null;
@@ -120,7 +123,28 @@ function click(d) {
     nodeList[d.id].children = nodeList[d.id]._children;
     nodeList[d.id]._children = null;
   }
-  //update();
+  recomputeNodes();
+  update();
+}
+
+function addChild(nodeID, parentID) {
+  nodes.push(originalNodes[nodeID]);
+
+  var parent = nodes.length - 1;
+  links.push({"source": parentID, "target": parent});
+
+  if (nodeList[nodeID] && nodeList[nodeID].children) {
+    for (var childIndex in nodeList[nodeID].children) {
+      var childID = originalNodes[nodeList[nodeID].children[childIndex]].id;
+      addChild(childID, parent);
+    }
+  }
+}
+
+function recomputeNodes() {
+  links = [];
+  nodes = [];
+  addChild(mainNode.id, 0);
 }
 
 function getSize(d) {
@@ -139,15 +163,4 @@ function tick() {
   node.attr("transform", function (d) {
     return "translate(" + (Math.max(d.radius, Math.min(width - d.radius, d.x))) + "," + (Math.max(d.radius, Math.min(height - d.radius, d.y))) + ")";
   });
-  labels.attr("transform", function (d) {
-    return "translate(" + (Math.max(d.radius, Math.min(width - d.radius, d.x))) + "," + (Math.max(d.radius, Math.min(height - d.radius, d.y))) + ")";
-  });
-}
-
-function dblclick(d) {
-  d3.select(this).classed("fixed", d.fixed = false);
-}
-
-function dragstart(d) {
-  d3.select(this).classed("fixed", d.fixed = true);
 }
